00746ea72683ac3f8bb8d429614ab2d5794b3f24
kaanzn <kaanozaydin35@icloud.com>
2025-06-21 12:28:22 +0200

    UI Change and Improvements

---
+++ asdas.cs
@@ -0,0 +1,10 @@
+<Button Classes="rectangle" 
+                HorizontalAlignment="Left"
+                VerticalAlignment="Top"
+                Width="40" Height="40"
+                CornerRadius="6"
+                Grid.Column="0" Grid.Row="0">
+                <Viewbox Width="30" Height="30">
+                        <Path Data="{StaticResource HorizontalLine}" Fill="#4c454c" Stretch="Uniform"/>
+                </Viewbox>
+        </Button>
